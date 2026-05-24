d172cfc0b59bf35dcad4f7ae25efc6567792f44a
Ziddan <113444408+Zidanadzi@users.noreply.github.com>
2026-05-24 20:52:25 +0700

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -18,10 +18,10 @@ extern int isapkrunning(char* pkgName);
 extern void initXMemoryTools(char* pkgName, char* rootMode);
 
 // Pustaka fungsi pencarian memori global Anda
-extern void SetRange(int rangeMode); 
-extern void MemorySearch(char* value, int type);
-extern void MemoryOffset(char* value, long offset, int type);
-extern void MemoryWrite(char* value, long offset, int type);
+extern int SetSearchRange(int type; 
+extern void MemorySearch(char* value, int TYPE);
+extern void MemoryOffset(char *value, long int offset, int type);
+extern void MemoryWrite(char *value, long int offset, int type);
 
 int main(int argc, char *argv[]) 
 {
