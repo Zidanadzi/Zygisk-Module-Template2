886adcf9e594485dd97819cdb0e6c69722575e1d
Ziddan <113444408+Zidanadzi@users.noreply.github.com>
2026-05-25 08:04:27 +0700

    Update module.cpp

--- module/src/main/cpp/module.cpp
+++ module/src/main/cpp/module.cpp
@@ -11,12 +11,11 @@
 #define LOG_TAG "ZygiskBridge"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-// Deklarasikan fungsi main dengan parameter tambahan untuk mengirim nama paket game
-extern int main(int argc, char *argv[], const char* gamePkg);
+// PERBAIKAN 1: Mengubah nama fungsi extern agar tidak melanggar aturan keyword 'main' C++
+extern int BukaFiturUtama(int argc, char *argv[], const char* gamePkg);
 
 void JalankanDiBackground(std::string nomor_case, std::string nama_game) {
-    // Memberikan jeda waktu 35 detik agar game benar-benar selesai melewati logo awal 
-    // dan Anti-Cheat mendarat dengan aman di layar masuk lobi
+    // Memberikan jeda waktu 35 detik agar game tenang di lobi utama sebelum eksekusi memori
     std::this_thread::sleep_for(std::chrono::seconds(35));
 
     char* arg_fitur = (char*)nomor_case.c_str(); 
@@ -27,10 +26,10 @@ void JalankanDiBackground(std::string nomor_case, std::string nama_game) {
     };
     int jumlah_argc = 2;
     
-    LOGI("Menjalankan main.cpp untuk game: %s dengan nomor Case: %s", nama_game.c_str(), arg_fitur);
+    LOGI("Menjalankan C++ Utama untuk game: %s dengan nomor Case: %s", nama_game.c_str(), arg_fitur);
     
-    // Eksekusi program utama Anda dengan mengirimkan nama paket game yang sudah valid
-    main(jumlah_argc, susunan_argv, nama_game.c_str());
+    // Panggil nama fungsi baru Anda yang aman
+    BukaFiturUtama(jumlah_argc, susunan_argv, nama_game.c_str());
 }
 
 class PerantaraZygisk : public zygisk::ModuleBase {
@@ -44,14 +43,12 @@ public:
         const char *processName = env->GetStringUTFChars(args->nice_name, nullptr);
         
         if (processName) {
-            // Memeriksa pakem target game secara akurat lewat memori internal Zygisk
             if (strcmp(processName, "com.tencent.ig") == 0 || 
                 strcmp(processName, "com.vng.pubgmobile") == 0 || 
                 strcmp(processName, "com.pubg.krmobile") == 0 || 
                 strcmp(processName, "com.rekoo.pubgm") == 0) 
             {
-                // Jalur penyimpanan file teks di dalam sistem root modul Magisk Anda
-                // ⚠️ GANTI "id_modul_anda" dengan ID yang tertulis di file module.prop Anda
+                // GANTI "id_modul_anda" dengan ID yang tertulis di file module.prop Anda
                 std::string path_file = "/data/adb/modules/template_module/pilihan.txt";
                 
                 std::string nomor_case = "1"; 
@@ -67,7 +64,6 @@ public:
                     }
                 }
 
-                // Kirim data nama game ke dalam fungsi thread latar belakang
                 std::string nama_game(processName);
                 std::thread(JalankanDiBackground, nomor_case, nama_game).detach();
             }
